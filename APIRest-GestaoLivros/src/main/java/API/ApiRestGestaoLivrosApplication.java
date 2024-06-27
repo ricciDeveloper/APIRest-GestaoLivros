d9265805efb24ea20e3543a944f8f6d223d72787
Joao Victor <joaovictor.ricci1999@gmail.com>
2024-06-27 11:10:48 -0300

    first commit

---
+++ APIRest-GestaoLivros/src/main/java/API/ApiRestGestaoLivrosApplication.java
@@ -0,0 +1,13 @@
+package API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestGestaoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestGestaoLivrosApplication.class, args);
+	}
+
+}
